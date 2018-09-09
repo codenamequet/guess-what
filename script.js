6d33c18d43aa28dac152caadeb060f3b7033c796
Marquet Reid <you@example.com>
2018-09-09 11:25:53 -0400

    added code to put data into html

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const words = ['HOUSTON', 'TEXAS', 'MONEY', 'TORONTO', 'UNIVERSITY', 'CONTENDERS
 let guess = [] // blank lines 
 let answer = [] // word being guessed each round
 let userGuess = [] // letter that the user chose
-let lettersGuessed = []
+let lettersGuessed = [] // letters that have been guessed so far
 let wrongGuess = 0
 
 // takes a random index of the words array and pushes place holders on the board for each letter 
@@ -12,11 +12,12 @@ const startRound = () => {
   answer = randomWord
   const boardSetUp = randomWord.forEach(() => guess.push('_'))
   const board = guess.join(' ')
+  document.getElementsByClassName('game-board')[0].innerHTML = board
 }
 
 startRound() // call this function in an event listener that handles starting the game
-// let userGuess = if letter clicked is right reveal it on the board else wrongGuess++ each click should refresh userGuess before pushing a new letter.
 
+//if the letter guessed is in lettersGuessed alert the user they already guessed that letter
 const handleUserGuess = document.addEventListener('keyup', (e) => {
   userGuess.push(e.key)
   console.log(`this is the letter:`, e.key)
@@ -27,13 +28,16 @@ const handleUserGuess = document.addEventListener('keyup', (e) => {
         guess[i] = userGuess[0]
       }
     }
+    document.getElementsByClassName('game-board')[0].innerHTML = guess
     lettersGuessed.push(userGuess[0])
+    document.getElementsByClassName('guesses')[0].innerHTML = `Letters Guessed So Far: ${lettersGuessed}`
     userGuess = []
   }
   else {
     wrongGuess++
     console.log(mindBlown())
     lettersGuessed.push(userGuess[0])
+    document.getElementsByClassName('guesses')[0].innerHTML = `Letters Guessed So Far: ${lettersGuessed}`
     userGuess = []
   }
 })
@@ -42,25 +46,25 @@ const handleUserGuess = document.addEventListener('keyup', (e) => {
 const mindBlown = () => {
   if (wrongGuess === 0) {
     console.log('😄')
-    return  '😄'
+    return  document.getElementsByClassName('emoji-container')[0].innerHTML = '😄'
   } else if (wrongGuess === 1) {
     console.log('😅')
-    return '😅'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '😅'
   } else if (wrongGuess === 2) {
     console.log('😫')
-    return '😫'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '😫'
   } else if (wrongGuess === 3) {
     console.log('😠')
-    return '😠'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '😠'
   } else if (wrongGuess === 4) {
     console.log('😡')
-    return '😡'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '😡'
   } else if (wrongGuess === 5) {
     console.log('🤬')
-    return '🤬'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '🤬'
   } else if (wrongGuess === 6) {
     console.log('🤯')
-    return '🤯'
+    return document.getElementsByClassName('emoji-container')[0].innerHTML = '🤯'
   }
 }
 
